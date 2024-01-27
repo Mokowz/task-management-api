9183039460f1a35baf82c67330bb7d0b2ee9a633
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-01-27 12:51:15 +0300

    Test task instance view and the CRUD operations

--- tasks/tests/test_views.py
+++ tasks/tests/test_views.py
@@ -4,7 +4,7 @@ from rest_framework.test import APIClient
 from django.urls import reverse
 
 
-class TaskTestCase(TestCase):
+class TaskListTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
 
@@ -58,6 +58,55 @@ class TaskTestCase(TestCase):
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]["title"], "Test Title 2")
 
+
+# Test the instance view
+class TaskInstanceTestView(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+        self.tag = Tag.objects.create(name="Test Tag")
+        self.task = Task.objects.create(
+            title = "Test Title",
+            description = 'Test Description',
+            completed = True,
+            created_at = '2024-01-01',
+            updated_at = '2024-01-04',
+            priority = 'high',
+            due_date = '2024-01-10',
+        )
+        self.task.tags.add(self.tag)
+
+    # Test the viewing function
+    def test_task_instance_get(self):
+        url = reverse("task-instance", kwargs={"pk": self.task.pk})
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    # Test the update functionality
+    def test_task_instance_update(self):
+        url = reverse("task-instance", kwargs={"pk": self.task.pk})
+        data = {
+            "title": "Updated Title",
+            "description": "Dummy description",
+            "completed": False,
+            "due_date": "2024-01-01",
+            "tags": self.tag.pk
+        }
+        response = self.client.put(url, data)
+        print(f"URL: {url}")
+        print(f"Response: {response}")
+        print(f"Response Data: {response.data}")
+
+        self.assertEqual(response.status_code, 200)
+        self.task.refresh_from_db()
+        self.assertEqual(self.task.title, "Updated Title")
     
+    # Test deletion
+    def test_task_instance_delete(self):
+        url = reverse("task-instance", kwargs={"pk": self.task.pk})
+        response = self.client.delete(url)
+
+        self.assertEqual(response.status_code, 204)
 
 
--- tasks/urls.py
+++ tasks/urls.py
@@ -5,6 +5,6 @@ from .views import TaskListView, TaskInstanceView, TaskSearchView
 
 urlpatterns = [
     path("tasks/", (TaskListView.as_view()), name="task-list"),
-    path("tasks/search/", cache_page(60 * 20)(TaskSearchView.as_view()), name="task-search"),
-    path("tasks/<int:pk>/", cache_page(60 * 20)(TaskInstanceView.as_view()), name="task"),
+    path("tasks/search/", (TaskSearchView.as_view()), name="task-search"),
+    path("tasks/<int:pk>/", (TaskInstanceView.as_view()), name="task-instance"),
 ]
