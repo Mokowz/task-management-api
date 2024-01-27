fb6a099cd2b84799918c1e0df665bfc3a03395d3
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-01-27 12:08:51 +0300

    Test filtering functionality

--- tasks/tests/test_views.py
+++ tasks/tests/test_views.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 class TaskTestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
+
         self.tag = Tag.objects.create(name="Test Tag")
         self.task = Task.objects.create(
             title = "Test Title",
@@ -19,29 +20,44 @@ class TaskTestCase(TestCase):
         )
         self.task.tags.add(self.tag)
 
+        self.task2 = Task.objects.create(
+            title = "Test Title 2",
+            description = 'Test Description 2',
+            completed = False,
+            created_at = '2024-04-01',
+            updated_at = '2024-05-04',
+            priority = 'low',
+            due_date = '2024-08-10',
+        )
+        self.task2.tags.add(self.tag)
+
     # test tasks list view (GET Method)
     def test_task_list_get_view(self):
         url = reverse('task-list')
         response = self.client.get(url)
+
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(len(response.data), 1)
+        self.assertEqual(len(response.data), 2)
         self.assertEqual(response.data[0]["title"], "Test Title")
         # self.assertEqual(response.data[0]["tags"], "Test Tag")
         self.assertEqual(response.data[0]["due_date"], "2024-01-10")
 
-    # Post method of list view
-    def test_task_list_post_view(self):
-        data = {
-            "title": "Test Title",
-            "description": 'Test Description',
-            "completed": True,
-            "created_at": '2024-01-01',
-            "updated_at": '2024-01-04',
-            "priority": 'high',
-            "due_date": '2024-01-10',
-        }
-        url = reverse('task-list')
-        response = self.client.post(url, data, format='json')
-        self.assertEqual(response.status_code, 201)
+    # Filter functionality
+    def test_task_list_filter_by_priority(self):
+        url = reverse("task-list")
+        response = self.client.get(url, {"priority": "low"})
+
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["title"], "Test Title 2")
+
+    # By completed
+    def test_task_list_filter_by_completed(self):
+        url = reverse("task-list")
+        response = self.client.get(url, {"completed": False})
+
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]["title"], "Test Title 2")
+
+    
 
 
