e610537c710110bfe9f432cc323674744c4c01dc
Ronny Mokaya <ronniemokaya30@gmail.com>
2024-02-01 15:32:28 +0300

    Complete user logged in signal receiver

--- accounts/signals.py
+++ accounts/signals.py
@@ -8,10 +8,11 @@ from django.template.loader import render_to_string
 @receiver(user_logged_in)
 def send_welcome_email(sender, request, user, **kwargs):
     subject = "Welcome to Task Management API"
+    body = f'{user.username} logged in'
     message = render_to_string(
         'accounts/welcome_email.html',
         {'user': user}
     )
     from_email = settings.EMAIL_HOST_USER
     recipient_list = [user.email, ]
-    send_mail(subject, message, from_email, recipient_list)
+    send_mail(subject=subject, message = body,from_email=from_email, recipient_list=recipient_list, html_message=message)
